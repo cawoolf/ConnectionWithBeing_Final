626f25064893751721877d1c777b2fe88097ed15
cawoolf <rayadevinfo@gmail.com>
2020-06-30 21:40:20 +0000

    First Square Menu version

--- app/src/main/java/com/example/connectionwithbeing/HomeFragment.java
+++ app/src/main/java/com/example/connectionwithbeing/HomeFragment.java
@@ -17,7 +17,6 @@ public class HomeFragment extends Fragment {
 
     private View mFragmentView;
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
