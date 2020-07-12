af42ba61c5341932190650f395bcee94b76bedc8
cawoolf <rayadevinfo@gmail.com>
2020-07-12 20:17:14 +0000

    Scroll View in nature

--- app/src/main/java/com/example/connectionwithbeing/NatureMenuActivity.java
+++ app/src/main/java/com/example/connectionwithbeing/NatureMenuActivity.java
@@ -1,16 +1,20 @@
 package com.example.connectionwithbeing;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.Toast;
 
 public class NatureMenuActivity extends AppCompatActivity {
+
 
     private LinearLayout mHomeButtonBar;
     private ImageView mHomeButton;
@@ -19,6 +23,9 @@ public class NatureMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nature_menu);
+
+
+
 
         mHomeButtonBar = findViewById(R.id.bottomNatureHomeButtonBar);
         mHomeButton = findViewById(R.id.bottomNatureHomeButton);
@@ -35,14 +42,14 @@ public class NatureMenuActivity extends AppCompatActivity {
     }
 
     //When back button on actionbar is pressed, returns to the previous activity which has not been destroyed.
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                Toast.makeText(getApplicationContext(),"Back button clicked", Toast.LENGTH_SHORT).show();
-                super.onBackPressed(); //This replicates the hard back button on the phone. Could just replace this with an Intent
-                break;
-        }
-        return true;
-    }
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        switch (item.getItemId()) {
+//            case android.R.id.home:
+//                Toast.makeText(getApplicationContext(),"Back button clicked", Toast.LENGTH_SHORT).show();
+//                super.onBackPressed(); //This replicates the hard back button on the phone. Could just replace this with an Intent
+//                break;
+//        }
+//        return true;
+//    }
 }
