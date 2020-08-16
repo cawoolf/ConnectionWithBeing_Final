417b1308279b68822d45754547b33d912d7b36ce
cawoolf <rayadevinfo@gmail.com>
2020-08-16 14:23:45 +0000

    Progress bar stuff

--- app/src/main/java/com/example/connectionwithbeing/QuoteActivity.java
+++ app/src/main/java/com/example/connectionwithbeing/QuoteActivity.java
@@ -3,6 +3,8 @@ package com.example.connectionwithbeing;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -13,17 +15,24 @@ public class QuoteActivity extends AppCompatActivity {
     private int i = 0;
     private TextView mTextView;
     private Handler mHandler = new Handler();
+    private Button mButton;
+    private boolean exitThread;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quote);
         mProgressBar = (ProgressBar) findViewById(R.id.pBar);
         mTextView = (TextView) findViewById(R.id.tView);
+        mButton = findViewById(R.id.btnShow);
 
-        Thread progressThread = new Thread(new Runnable() {
+        final Thread progressThread = new Thread(new Runnable() {
             @Override
             public void run() {
-                while (i < 100) {
+                exitThread = false;
+                while (i < 100 && exitThread == false) {
                     i += 1;
                     // Update the progress bar and display the current value in text view
                     mHandler.post(new Runnable() {
@@ -45,6 +54,18 @@ public class QuoteActivity extends AppCompatActivity {
         });
 
         progressThread.start();
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                progressThread.interrupt();
+                stopThread();
+                startExercise();
+                onPause();
+                onDestroy();
+//                finish();
+            }
+        });
 //        try {
 //            progressThread.join();
 //            startExercise();
@@ -53,6 +74,10 @@ public class QuoteActivity extends AppCompatActivity {
 //        }
     }
 
+    public void stopThread()
+    {
+        exitThread = true;
+    }
 
     public void startExercise() {
         Intent exerciseIntent = getIntent();
