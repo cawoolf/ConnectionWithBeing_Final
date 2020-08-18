492c95f9ac989a3053b6d04414997ba2fbab7f7d
cawoolf <rayadevinfo@gmail.com>
2020-08-18 15:39:41 +0000

    Animation welcome screen.

--- app/src/main/java/com/example/connectionwithbeing/WelcomeActivity.java
+++ app/src/main/java/com/example/connectionwithbeing/WelcomeActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -13,24 +16,25 @@ public class WelcomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
+
+        ImageView welcomeScreenImage = findViewById(R.id.WelcomeActivity_ImageView);
+        Animation rotateAnimation =
+                AnimationUtils.loadAnimation(getApplicationContext(),
+                        R.anim.rotate);
+
+        welcomeScreenImage.startAnimation(rotateAnimation);
+
+
         new Handler().postDelayed(new Runnable() {
-
-// Using handler with postDelayed called runnable run method
-
             @Override
-
             public void run() {
 
                 Intent i = new Intent(WelcomeActivity.this, MainActivity.class);
 
                 startActivity(i);
-
                 // close this activity
-
                 finish();
-
             }
-
         }, 3000); // wait for 5 seconds
     }
 }
