cfee3c1f7b01957c756c093f06bbf5ac47665fb3
cawoolf <rayadevinfo@gmail.com>
2020-09-10 14:27:26 -0600

    activity update2

--- app/src/main/java/com/example/connectionwithbeing/IntroductionActivity2.java
+++ app/src/main/java/com/example/connectionwithbeing/IntroductionActivity2.java
@@ -1,10 +1,24 @@
 package com.example.connectionwithbeing;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class IntroductionActivity2 extends AppCompatActivity {
+
+    private LinearLayout mIntroLinearLayout, mIntroExerciseMenu, mIntroExerciseIcon, mIntroNext2Button;
+    private TextView mIntroIconTextView;
+
+    private int clickCount = 2;
+
+    private int[] introImages = {R.id.UIIntroExerciseMenuIcon_LinearLayout, R.id.UIIntroExerciseIcon_LinearLayout,
+            R.drawable.guihome, R.drawable.guibookmark, R.drawable.shuffleiconimage, R.drawable.guiinfoitemimage};
+
+    int[] introText = {R.string.ui_menu_icon, R.string.ui_exercise_icon, R.string.ui_home_icon, R.string.ui_bookmark_icon,
+            R.string.ui_shuffle_icon, R.string.ui_info_icon};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
